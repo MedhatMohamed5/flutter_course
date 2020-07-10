5197d13b3a97857fcb0bbe044299cb3ffe0638f6
Medhat Mohamed <35978271+MedhatMohamed5@users.noreply.github.com>
2020-07-10 22:02:12 +0200

    Drawer before navigation definition

--- lib/shared_ui/navigation_drawer.dart
+++ lib/shared_ui/navigation_drawer.dart
@@ -6,8 +6,44 @@ class NavigationDrawer extends StatefulWidget {
 }
 
 class _NavigationDrawerState extends State<NavigationDrawer> {
+  List<String> navMenu = [
+    'Explore',
+    'Headline news',
+    'Read later',
+    'Videos',
+    'Photos',
+    'Settings',
+    'Logout'
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 32, left: 16),
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4),
+              child: ListTile(
+                onTap: () {},
+                title: Text(
+                  navMenu[index].trim().toUpperCase(),
+                  style: TextStyle(
+                    color: Colors.grey.shade700,
+                    fontSize: 22,
+                  ),
+                ),
+                trailing: Icon(
+                  Icons.chevron_right,
+                  color: Colors.grey.shade500,
+                ),
+              ),
+            );
+          },
+          itemCount: navMenu.length,
+        ),
+      ),
+    );
   }
 }
