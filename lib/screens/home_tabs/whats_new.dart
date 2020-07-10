f4a4b2e3f5f4a731fa7ced0efbde233506bb4a1c
Medhat Mohamed <35978271+MedhatMohamed5@users.noreply.github.com>
2020-07-10 02:00:54 +0200

    What's New static design

--- lib/screens/home_tabs/whats_new.dart
+++ lib/screens/home_tabs/whats_new.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class WhatsNew extends StatefulWidget {
@@ -8,10 +9,237 @@ class WhatsNew extends StatefulWidget {
 class _WhatsNewState extends State<WhatsNew> {
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _header(),
+          _stories(),
+        ],
+      ),
+    );
+  }
 
-      child: Container(
-        color: Colors.tealAccent,
+  Widget _header() {
+    TextStyle _headerTitle = TextStyle(
+        color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold);
+    TextStyle _headerDesc = TextStyle(
+      color: Colors.white,
+      fontSize: 16,
+    );
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.25,
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Free Text',
+                style: _headerTitle,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                'Lorem ipsum is placeholder text commonly used in the graphic, '
+                'print, and publishing industries for previewing layouts and visual mockups',
+                style: _headerDesc,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: ExactAssetImage('assets/images/placeholder_bg.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget _stories() {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: _sectionTitle('Top Stories'),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Card(
+              child: Column(
+                children: <Widget>[
+                  _drawSingleRow(),
+                  _drawDivider(),
+                  _drawSingleRow(),
+                  _drawDivider(),
+                  _drawSingleRow(),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  child: _sectionTitle('Recent updates'),
+                ),
+                _drawRecentUpdatesCard(Colors.deepOrange),
+                _drawRecentUpdatesCard(Colors.tealAccent),
+                SizedBox(
+                  height: 32,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _drawSingleRow() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: <Widget>[
+          SizedBox(
+            child: Image(
+              image: ExactAssetImage('assets/images/placeholder_bg.png'),
+              fit: BoxFit.cover,
+            ),
+            height: 128,
+            width: 128,
+          ),
+          SizedBox(
+            width: 16,
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'The world global warming annual summit',
+                  maxLines: 2,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('Medhat Mohamed'),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.timer),
+                        Text('15 min'),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _drawDivider() {
+    return Container(
+      height: 1,
+      color: Colors.grey.shade300,
+      width: MediaQuery.of(context).size.width * .70,
+    );
+  }
+
+  Widget _sectionTitle(String title) {
+    return Text(
+      title,
+      style: TextStyle(
+          color: Colors.grey.shade800,
+          fontWeight: FontWeight.w500,
+          fontSize: 16),
+    );
+  }
+
+  Widget _drawRecentUpdatesCard(Color color) {
+    return Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: ExactAssetImage('assets/images/placeholder_bg.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * .25,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 4),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: color,
+              ),
+              child: Text(
+                'sport'.toUpperCase(),
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                  letterSpacing: 2,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 8, left: 16, right: 16),
+            child: Text(
+              'Sports title for sport news',
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: Row(
+              children: <Widget>[
+                Icon(
+                  Icons.timer,
+                  color: Colors.grey,
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Text(
+                  '15 mins',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
