b0aa9540f4b2817d91a01dda6e39cfe950fae1b5
Medhat Mohamed <35978271+MedhatMohamed5@users.noreply.github.com>
2020-07-11 18:01:48 +0200

    Add categories_api

---
+++ lib/api/categories_api.dart
@@ -0,0 +1,12 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_course/models/category.dart';
+import 'package:flutter_course/utilities/api_utilities.dart';
+
+class CategoriesAPI {
+  Future<List<Category>> fetchCategories() async {
+    List<Category> categories = List<Category>();
+
+    return categories;
+  }
+}
